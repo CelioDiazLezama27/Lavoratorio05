1283de2c0537e6dad9b34f9749ae5c4f33e5865c
CelioDiazLezama27 <142622900+CelioDiazLezama27@users.noreply.github.com>
2023-09-19 23:59:06 -0500

    Add files via upload

---
+++ src/laboratorio05/Laboratorio05.java
@@ -0,0 +1,15 @@
+
+package laboratorio05;
+
+/**
+ *
+ * @author Celio D
+ */
+public class Laboratorio05 {
+
+   
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
